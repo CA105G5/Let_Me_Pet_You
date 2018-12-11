96d22792d0906dc89cc8e5fecde988bda57b3f5a
Zoey <Zoey@MSI>
2018-12-11 21:39:48 +0800

    test2

--- src/test/Test.java
+++ src/test/Test.java
@@ -4,10 +4,7 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("哈囉");
 		System.out.println("最新");
-
-
 		System.out.println("yooo2");
-
 		System.out.println("yooo");
 
 	}
