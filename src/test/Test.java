f38f76ddd9e86a5145692dcea06e588dafc2c734
spwang <wspishandsome@gmail.com>
2018-12-11 21:31:36 +0800

    rrrrr

--- src/test/Test.java
+++ src/test/Test.java
@@ -4,12 +4,7 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("哈囉");
 		System.out.println("最新");
-<<<<<<< HEAD
-		System.out.println("yooo");
-=======
-		System.out.println("12334");
-		
->>>>>>> branch 'master' of https://github.com/CA105G5/Let_Me_Pet_You.git
+
 	}
 
 }
