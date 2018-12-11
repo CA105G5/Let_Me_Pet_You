1f14b81ac32ce67b5d2ee1ebf589eb6bf19334df
Zoey <Zoey@MSI>
2018-12-11 21:39:29 +0800

    本地端測試

--- src/test/Test.java
+++ src/test/Test.java
@@ -4,13 +4,12 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("哈囉");
 		System.out.println("最新");
-<<<<<<< HEAD
+
 
 		System.out.println("yooo2");
-=======
 
 		System.out.println("yooo");
->>>>>>> branch 'master' of https://github.com/CA105G5/Let_Me_Pet_You.git
+
 	}
 
 }
