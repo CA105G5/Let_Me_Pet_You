862bb164177e38076aa95bd1ec403fc6c1d4467e
Java <Java@DESKTOP-TQ00GH9>
2018-12-13 22:25:13 +0800

    差main方法

--- src/com/rscMsgRt/model/RscMsgRtJDBCDAO.java
+++ src/com/rscMsgRt/model/RscMsgRtJDBCDAO.java
@@ -1,37 +1,307 @@
 package com.rscMsgRt.model;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
-public class RscMsgRtJDBCDAO implements RscMsgRtDAO_interface {
+import com.rscRt.model.RscRtVO;
 
+public class RscMsgRtJDBCDAO implements RscMsgRtDAO_interface {
+	String driver = "oracle.jdbc.driver.OracleDriver";
+	String url = "jdbc:oracle:thin:@localhost:1521:XE";
+	String userid = "CA105G5";
+	String passwd = "123456";
+	
+	private static final String INSERT_STMT = 
+			"INSERT INTO RSC_MSG_RT (rsc_msg_rt_id,rsc_msg_id,memb_id,rsc_msg_rt_time,rsc_msg_rt_comm,rsc_msg_rv_des,rsc_msg_rt_status) VALUES ('RMR'||LPAD(to_char(rsc_rt_seq.NEXTVAL), 12, '0'),?,?,?,?,?,?)";
+	private static final String GET_ALL_STMT = 
+			"SELECT rsc_msg_rt_id,rsc_msg_id,memb_id,to_char(rsc_msg_rt_time,'yyyy-mm-dd hh24:mi:ss') rsc_msg_rt_time,rsc_msg_rt_comm,rsc_msg_rv_des,rsc_msg_rt_status FROM RSC_MSG_RT order by rsc_msg_rt_id";
+	private static final String GET_ONE_STMT = 
+			"SELECT rsc_msg_rt_id,rsc_msg_id,memb_id,to_char(rsc_msg_rt_time,'yyyy-mm-dd hh24:mi:ss') rsc_msg_rt_time,rsc_msg_rt_comm,rsc_msg_rv_des,rsc_msg_rt_status FROM RSC_MSG_RT where rsc_msg_rt_id = ?";
+	private static final String DELETE = 
+			"DELETE FROM RSC_MSG_RT where rsc_msg_rt_id = ?";
+	private static final String UPDATE = 
+			"UPDATE RSC_MSG_RT set rsc_msg_id = ?,memb_id = ?,rs_msgc_rt_time = ?,rsc_msg_rt_comm = ?,rsc_msg_rv_des = ?,rsc_msg_rt_status = ? where rsc_msg_rt_id = ?";
+	
 	@Override
 	public void insert(RscMsgRtVO rscMsgRtVO) {
-		// TODO Auto-generated method stub
+		Connection con = null;
+		PreparedStatement pstmt = null;
+
+		try {
+
+			Class.forName(driver);
+			con = DriverManager.getConnection(url, userid, passwd);
+			pstmt = con.prepareStatement(INSERT_STMT);
+
+			pstmt.setString(1, rscMsgRtVO.getRsc_msg_id());
+			pstmt.setString(2, rscMsgRtVO.getMemb_id());
+			pstmt.setTimestamp(3, rscMsgRtVO.getRsc_msg_rt_time());
+			pstmt.setString(4, rscMsgRtVO.getRsc_msg_rt_comm());
+			pstmt.setString(5, rscMsgRtVO.getRsc_msg_rv_des());
+			pstmt.setString(6, rscMsgRtVO.getRsc_msg_rt_status());
+		
+
+			int rowsUpdated =pstmt.executeUpdate();
+			System.out.println("Changed " + rowsUpdated + "rows");
+
+			// Handle any driver errors
+		} catch (ClassNotFoundException e) {
+			throw new RuntimeException("Couldn't load database driver. "
+					+ e.getMessage());
+			// Handle any SQL errors
+		} catch (SQLException se) {
+			throw new RuntimeException("A database error occured. "
+					+ se.getMessage());
+			// Clean up JDBC resources
+		} finally {
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (SQLException se) {
+					se.printStackTrace(System.err);
+				}
+			}
+			if (con != null) {
+				try {
+					con.close();
+				} catch (Exception e) {
+					e.printStackTrace(System.err);
+				}
+			}
+		}
+		
 		
 	}
 
 	@Override
 	public void update(RscMsgRtVO rscMsgRtVO) {
-		// TODO Auto-generated method stub
+		Connection con = null;
+		PreparedStatement pstmt = null;
+
+		try {
+
+			Class.forName(driver);
+			con = DriverManager.getConnection(url, userid, passwd);
+			pstmt = con.prepareStatement(UPDATE);
+
+			pstmt.setString(1, rscMsgRtVO.getRsc_msg_id());
+			pstmt.setString(2, rscMsgRtVO.getMemb_id());
+			pstmt.setTimestamp(3, rscMsgRtVO.getRsc_msg_rt_time());
+			pstmt.setString(4, rscMsgRtVO.getRsc_msg_rt_comm());
+			pstmt.setString(5, rscMsgRtVO.getRsc_msg_rv_des());
+			pstmt.setString(6, rscMsgRtVO.getRsc_msg_rt_status());
+			pstmt.setString(7, rscMsgRtVO.getRsc_msg_rt_id());
+			
+
+			int rowsUpdated = pstmt.executeUpdate();
+			System.out.println("Changed " + rowsUpdated + "rows");
+
+			// Handle any driver errors
+		} catch (ClassNotFoundException e) {
+			throw new RuntimeException("Couldn't load database driver. "
+					+ e.getMessage());
+			// Handle any SQL errors
+		} catch (SQLException se) {
+			throw new RuntimeException("A database error occured. "
+					+ se.getMessage());
+			// Clean up JDBC resources
+		} finally {
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (SQLException se) {
+					se.printStackTrace(System.err);
+				}
+			}
+			if (con != null) {
+				try {
+					con.close();
+				} catch (Exception e) {
+					e.printStackTrace(System.err);
+				}
+			}
+		}
+
+		
 		
 	}
 
 	@Override
 	public void delete(String rsc_msg_rt_id) {
-		// TODO Auto-generated method stub
-		
+		Connection con = null;
+		PreparedStatement pstmt = null;
+
+		try {
+
+			Class.forName(driver);
+			con = DriverManager.getConnection(url, userid, passwd);
+			pstmt = con.prepareStatement(DELETE);
+
+			pstmt.setString(1,rsc_msg_rt_id);
+
+			int rowsUpdated = pstmt.executeUpdate();
+			System.out.println("Changed " + rowsUpdated + "rows");
+			
+			// Handle any driver errors
+		} catch (ClassNotFoundException e) {
+			throw new RuntimeException("Couldn't load database driver. "
+					+ e.getMessage());
+			// Handle any SQL errors
+		} catch (SQLException se) {
+			throw new RuntimeException("A database error occured. "
+					+ se.getMessage());
+			// Clean up JDBC resources
+		} finally {
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (SQLException se) {
+					se.printStackTrace(System.err);
+				}
+			}
+			if (con != null) {
+				try {
+					con.close();
+				} catch (Exception e) {
+					e.printStackTrace(System.err);
+				}
+			}
+		}
+
 	}
 
 	@Override
 	public RscMsgRtVO findByPrimaryKey(String rsc_msg_rt_id) {
-		// TODO Auto-generated method stub
-		return null;
+		RscMsgRtVO rscMsgRtVO = null;
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+
+		try {
+
+			Class.forName(driver);
+			con = DriverManager.getConnection(url, userid, passwd);
+			pstmt = con.prepareStatement(GET_ONE_STMT);
+
+			pstmt.setString(1,rsc_msg_rt_id);
+
+			rs = pstmt.executeQuery();
+
+			while (rs.next()) {
+				// regionVo 嘿 Domain objects
+				rscMsgRtVO = new RscMsgRtVO();
+				rscMsgRtVO.setRsc_msg_rt_id(rs.getString("rsc_rt_id"));
+				rscMsgRtVO.setRsc_msg_id(rs.getString("rsc_id"));
+				rscMsgRtVO.setMemb_id(rs.getString("memb_id"));
+				rscMsgRtVO.setRsc_msg_rt_time(rs.getTimestamp("rsc_rt_time"));
+				rscMsgRtVO.setRsc_msg_rt_comm(rs.getString("rsc_rt_comm"));
+				rscMsgRtVO.setRsc_msg_rv_des(rs.getString("rsc_rv_des"));
+				rscMsgRtVO.setRsc_msg_rt_status(rs.getString("rsc_rt_status"));
+				
+				
+			}
+
+			// Handle any driver errors
+		} catch (ClassNotFoundException e) {
+			throw new RuntimeException("Couldn't load database driver. "
+					+ e.getMessage());
+			// Handle any SQL errors
+		} catch (SQLException se) {
+			throw new RuntimeException("A database error occured. "
+					+ se.getMessage());
+			// Clean up JDBC resources
+		} finally {
+			if (rs != null) {
+				try {
+					rs.close();
+				} catch (SQLException se) {
+					se.printStackTrace(System.err);
+				}
+			}
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (SQLException se) {
+					se.printStackTrace(System.err);
+				}
+			}
+			if (con != null) {
+				try {
+					con.close();
+				} catch (Exception e) {
+					e.printStackTrace(System.err);
+				}
+			}
+		}
+		return rscMsgRtVO;
 	}
 
 	@Override
 	public List<RscMsgRtVO> getAll() {
-		// TODO Auto-generated method stub
-		return null;
+		List<RscMsgRtVO> list = new ArrayList<RscMsgRtVO>();
+		RscMsgRtVO rscMsgRtVO = null;
+
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+
+		try {
+
+			Class.forName(driver);
+			con = DriverManager.getConnection(url, userid, passwd);
+			pstmt = con.prepareStatement(GET_ALL_STMT);
+			rs = pstmt.executeQuery();
+
+			while (rs.next()) {
+				// empVO 嘿 Domain objects
+				rscMsgRtVO = new RscMsgRtVO();
+				rscMsgRtVO.setRsc_msg_rt_id(rs.getString("rsc_rt_id"));
+				rscMsgRtVO.setRsc_msg_id(rs.getString("rsc_id"));
+				rscMsgRtVO.setMemb_id(rs.getString("memb_id"));
+				rscMsgRtVO.setRsc_msg_rt_time(rs.getTimestamp("rsc_rt_time"));
+				rscMsgRtVO.setRsc_msg_rt_comm(rs.getString("rsc_rt_comm"));
+				rscMsgRtVO.setRsc_msg_rv_des(rs.getString("rsc_rv_des"));
+				list.add(rscMsgRtVO); // Store the row in the list
+			}
+
+			// Handle any driver errors
+		} catch (ClassNotFoundException e) {
+			throw new RuntimeException("Couldn't load database driver. "
+					+ e.getMessage());
+			// Handle any SQL errors
+		} catch (SQLException se) {
+			throw new RuntimeException("A database error occured. "
+					+ se.getMessage());
+			// Clean up JDBC resources
+		} finally {
+			if (rs != null) {
+				try {
+					rs.close();
+				} catch (SQLException se) {
+					se.printStackTrace(System.err);
+				}
+			}
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (SQLException se) {
+					se.printStackTrace(System.err);
+				}
+			}
+			if (con != null) {
+				try {
+					con.close();
+				} catch (Exception e) {
+					e.printStackTrace(System.err);
+				}
+			}
+		}
+		return list;
 	}
 
 }
--- src/com/rscMsgRt/model/RscMsgRtVO.java
+++ src/com/rscMsgRt/model/RscMsgRtVO.java
@@ -7,9 +7,9 @@ public class RscMsgRtVO implements java.io.Serializable{
 	private String rsc_msg_rt_id;
 	private String rsc_msg_id;
 	private String memb_id;
-	private Timestamp rsc_msg_rt_date;
+	private Timestamp rsc_msg_rt_time;
 	private String rsc_msg_rt_comm;
-	private String rsc_msg_rt_des;
+	private String rsc_msg_rv_des;
 	private String rsc_msg_rt_status;
 	public String getRsc_msg_rt_id() {
 		return rsc_msg_rt_id;
@@ -29,11 +29,11 @@ public class RscMsgRtVO implements java.io.Serializable{
 	public void setMemb_id(String memb_id) {
 		this.memb_id = memb_id;
 	}
-	public Timestamp getRsc_msg_rt_date() {
-		return rsc_msg_rt_date;
+	public Timestamp getRsc_msg_rt_time() {
+		return rsc_msg_rt_time;
 	}
-	public void setRsc_msg_rt_date(Timestamp rsc_msg_rt_date) {
-		this.rsc_msg_rt_date = rsc_msg_rt_date;
+	public void setRsc_msg_rt_time(Timestamp rsc_msg_rt_time) {
+		this.rsc_msg_rt_time = rsc_msg_rt_time;
 	}
 	public String getRsc_msg_rt_comm() {
 		return rsc_msg_rt_comm;
@@ -41,11 +41,11 @@ public class RscMsgRtVO implements java.io.Serializable{
 	public void setRsc_msg_rt_comm(String rsc_msg_rt_comm) {
 		this.rsc_msg_rt_comm = rsc_msg_rt_comm;
 	}
-	public String getRsc_msg_rt_des() {
-		return rsc_msg_rt_des;
+	public String getRsc_msg_rv_des() {
+		return rsc_msg_rv_des;
 	}
-	public void setRsc_msg_rt_des(String rsc_msg_rt_des) {
-		this.rsc_msg_rt_des = rsc_msg_rt_des;
+	public void setRsc_msg_rv_des(String rsc_msg_rv_des) {
+		this.rsc_msg_rv_des = rsc_msg_rv_des;
 	}
 	public String getRsc_msg_rt_status() {
 		return rsc_msg_rt_status;
@@ -53,5 +53,6 @@ public class RscMsgRtVO implements java.io.Serializable{
 	public void setRsc_msg_rt_status(String rsc_msg_rt_status) {
 		this.rsc_msg_rt_status = rsc_msg_rt_status;
 	}
+
 	
 }
