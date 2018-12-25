f33df91a9218747f5ea9139af1185fe0ec9d3129
Larayang <jjuies424@gmail.com>
2018-12-25 16:13:24 +0800

    追蹤的救援複合查詢

--- src/com/rscTrack/model/RscTrackService.java
+++ src/com/rscTrack/model/RscTrackService.java
@@ -1,5 +1,33 @@
 package com.rscTrack.model;
 
+import java.util.List;
+import java.util.Map;
+
+
+
 public class RscTrackService {
+	
+	private RscTrackDAO_interface dao;
+	
+	public RscTrackService() {
+		dao = new RscTrackJDBCDAO();
+	}
+	public void addRscTrack(RscTrackVO rscTrackVO) {
+		
+		dao.insert(rscTrackVO);
+	}
+	
+	public void deleteRscTrack(String rsc_id,String memb_id) {
+		dao.delete(rsc_id,memb_id);
+	}
+	
+	
+	public List<RscTrackVO> getAll(){
+		return dao.getAll();
+	}
+	
+	public List<RscTrackVO> getAll(Map<String ,String[]> map){
+		return dao.getAll(map);
+	}
 
 }
