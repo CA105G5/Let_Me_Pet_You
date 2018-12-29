c98923540c3c03876cd628826f21076b69dbee7b
Larayang <jjuies424@gmail.com>
2018-12-29 19:55:03 +0800

    救援

--- src/com/rescue/controller/RescueServlet.java
+++ src/com/rescue/controller/RescueServlet.java
@@ -1,5 +1,102 @@
 package com.rescue.controller;
 
-public class RescueServlet {
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.*;
+
+
+import com.rescue.model.*;
+
+public class RescueServlet extends HttpServlet{
+	
+	public void doGet(HttpServletRequest req, HttpServletResponse res)
+			throws ServletException, IOException {
+		doPost(req, res);
+	}
+
+	public void doPost(HttpServletRequest req, HttpServletResponse res)
+			throws ServletException, IOException {
+		
+		req.setCharacterEncoding("UTF-8");
+		String action = req.getParameter("action");
+		
+		if ("getOne_For_Display".equals(action)) { // 來自select_page.jsp的請求
+
+			List<String> errorMsgs = new LinkedList<String>();
+			// Store this set in the request scope, in case we need to
+			// send the ErrorPage view.
+			req.setAttribute("errorMsgs", errorMsgs);
+
+			try {
+				/***************************1.接收請求參數 - 輸入格式的錯誤處理**********************/
+				String str = req.getParameter("rsc_id");
+//				if (str == null || (str.trim()).length() == 0) {
+//					errorMsgs.add("請輸入員工編號");
+//				}
+				// Send the use back to the form, if there were errors
+//				if (!errorMsgs.isDonatety()) {
+//					RequestDispatcher failureView = req
+//							.getRequestDispatcher("/emp/select_page.jsp");
+//					failureView.forward(req, res);
+//					return;//程式中斷
+//				}
+				
+//				Integer empno = null;
+//				try {
+//					empno = new Integer(str);
+//				} catch (Exception e) {
+//					errorMsgs.add("員工編號格式不正確");
+//				}
+//				// Send the use back to the form, if there were errors
+//				if (!errorMsgs.isDonatety()) {
+//					RequestDispatcher failureView = req
+//							.getRequestDispatcher("/emp/select_page.jsp");
+//					failureView.forward(req, res);
+//					return;//程式中斷
+//				}
+				
+				/***************************2.開始查詢資料*****************************************/
+				RescueService rscSvc = new RescueService();
+				RescueVO rescueVO = rscSvc.getOneRescue(str);
+				if (rescueVO == null) {
+					errorMsgs.add("查無資料");
+				}
+				// Send the use back to the form, if there were errors
+				if (!errorMsgs.isEmpty()) {
+					RequestDispatcher failureView = req
+							.getRequestDispatcher("/front-end/rescue/listAllRescue.jsp");
+					failureView.forward(req, res);
+					return;//程式中斷
+				}
+				
+				/***************************3.查詢完成,準備轉交(Send the Success view)*************/
+				req.setAttribute("rescueVO", rescueVO); // 資料庫取出的donateVO物件,存入req
+				String url = "/front-end/rescue/listOneRescue.jsp";
+				RequestDispatcher successView = req.getRequestDispatcher(url); // 成功轉交 listOneDonate.jsp
+				successView.forward(req, res);
+
+				/***************************其他可能的錯誤處理*************************************/
+			} catch (Exception e) {
+				errorMsgs.add("無法取得資料:" + e.getMessage());
+				RequestDispatcher failureView = req
+						.getRequestDispatcher("/front-end/rescue/listAllRescue.jsp");
+				failureView.forward(req, res);
+			}
+		}
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
 }
