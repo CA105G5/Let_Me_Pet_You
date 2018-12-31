9f0f7b33119ccc33353f47352746785942aa064c
spwang <wspishandsome@gmail.com>
2018-12-31 14:55:13 +0800

    ttest

--- src/com/CurrencyDetail/model/CurDtDAO_interface.java
+++ src/com/CurrencyDetail/model/CurDtDAO_interface.java
@@ -2,10 +2,13 @@ package com.CurrencyDetail.model;
 
 import java.util.List;
 
+import com.pet.model.PetVO;
+
 public interface CurDtDAO_interface {
 	public void insert(CurDtVO curdtVO);
 	public void update(CurDtVO curdtVO);
 	public void delete(String cur_dt_id);
 	public CurDtVO findByPrimaryKey(String cur_dt_id);
 	public List<CurDtVO> getAll();
+	public List<CurDtVO> getAllCurDtsFromSameMember(String memb_id);
 }
---
+++ src/com/CurrencyDetail/model/CurDtService.java
@@ -0,0 +1,16 @@
+package com.CurrencyDetail.model;
+
+import java.util.List;
+
+import com.pet.model.PetDAO_interface;
+import com.pet.model.PetVO;
+
+public class CurDtService {
+	private CurDtDAO_interface dao;
+	public CurDtService() {
+		dao = new CurDtJDBCDAO();
+	}
+	public List<CurDtVO> getAllCurDtsFromSameMember(String memb_id){
+		return dao.getAllCurDtsFromSameMember(memb_id);
+	}
+}
