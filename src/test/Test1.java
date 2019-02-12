5946439dbe2a15bfeb6b4070b7470bc019c66cf1
spwang <wspishandsome@gmail.com>
2019-02-12 21:03:24 +0800

    test1

---
+++ src/test/Test1.java
@@ -0,0 +1,28 @@
+package test;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		int [] num = {56,24,32,16};
+		System.out.println(Test1.gcd(num));
+	}
+	public static int gcd(int [] number) {
+		int answer = -1;
+		//答案
+		Boolean check = true;
+		for(int i = 1 ;i<number[0];i++) {
+			check=true;
+			for(int j=0 ; j<number.length;j++) {
+				if(number[j]%i != 0) {
+					check = false;
+				}
+			}
+			if(check) {
+				answer = i ;
+			}
+		}
+		//答案
+		return answer;
+	}
+
+}
